163126692f02d902d335682e376ded1ab742a379
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-24 01:33:22 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,9 +11,9 @@ if HEROKU:
 if not HEROKU:
 
     bot_token = "6226805699:AAE1T96RKSjs06kNID7VW9xVsYaSOFDI92o"
-    api_id2=24449619
-    api_hash2="70bab6764c1e34eae004925f8ec0e122"
-    session_string="BQF1ElMAmYJyE8YVm-k41BNYrdTjPHGrR3r6yXOI2ly6FirqWNJPtxPXkXoQ0BcrATqe8n6FvLo8tw4wQkUE_bUfYP9QA5k25CsVNqFlX1zs4QWdgjyCu_9gYxAMroaYWkbJJrcS9JpmOFnz5fHD9gUyCU3OK4n0tEOSmcLmkNw3Tt7f7wMr6f5Jt6YJAKI6jHyPeZLZv8dvRpl8cMhTt5nt_adknorYB5jH2QKnEV7utW9QNtQNDPTCo98QREXuoQcIHSUX7DdtKzhZDzWA1j1oxQRZ7-9tL3gjY2f2kkO45qKxmwAR4yKs5tLBO4Q3jSagWMZhCZ4x95E5VIe0aZY0hD5gKgAAAAGI5tIhAA"
+    api_id2=13958802
+    api_hash2="f6d2d3b04309e9660a0be5b2a6195a7c"
+    session_string="BQDU_pIAXoKacHY7FmwGEWTozwOEjLbdZqf_c2yYKGElslXEdnmDkcs7VE4-hUiwfXGlmVWRroKat2PPVuj-oT8Fj9pSTOaKPTdDH9lxIOLukYsAnLqjafv9RPduahpX7b8aeXdR71d_1kNwxIy39l874vZz8jFZIi6R_kgp_qacX7s_pF-4eGQDszfmIR6flqXRNls4-DAsgJzRtIC1HlF96rPWQBA-V38e9OUd9yXRHP24pnGadzNDV3PC9s24wRowxctuV3ZWnWQfuBpiCfb1jBsvmWd6j3snbPxsjuYM_0KH9MbYWVUnszOce3Nm7jTksEMMhbMsejtZBrQGkVGCVs3bJAAAAAF3J43eAA"
     ARQ_API_KEY = "QKILRQ-VUTFDL-MQBWZA-EGQFPM-ARQ" #"LCZFSR-XCJSNJ-JVHHXC-UUVLWE-ARQ"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
