a1c79348ed84d8df10ac3a7f3c82f10823e3756d
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-24 03:44:18 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -76,13 +76,14 @@ async def type_and_send3(_, message):
 
 @luna.on_message(filters.reply & filters.sticker & filters.group)
 async def type_and_send4(_, message):
-    chat_id = message.chat.id
-    user_id = message.from_user.id if message.from_user else 0
-    #query = message.text.strip()
-    await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
-    response = "hãy nói tiếng người đừng gửi sticker"
-    await message.reply_text(response)
-    await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
+    if filters.sticker
+        chat_id = message.chat.id
+        user_id = message.from_user.id if message.from_user else 0
+        #query = message.text.strip()
+        await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
+        response = "hãy nói tiếng người đừng gửi sticker"
+        await message.reply_text(response)
+        await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
 
 @luna.on_message(filters.reply & filters.animation & filters.group)
 async def type_and_send5(_, message):
