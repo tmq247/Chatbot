36728283ec5739085526578403ad28012d33449b
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-12-24 02:00:11 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -62,6 +62,15 @@ async def type_and_send2(_, message):
     await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
 
 
+@luna.on_message(filters.photo & filters.reply or filters.sticker & filters.reply or filters.animation & filters.reply or filters.video & filters.reply or filters.voice & filters.reply)
+async def type_and_send3(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
+    response, _ = await gather(lunaQuery(query, user_id), sleep(2))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
 
 repo = filters.command("repo")
 @luna.on_message(repo)
