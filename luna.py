ea77459eb895fc3956f1822dff45df68308d5c97
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-12 17:11:40 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -62,7 +62,7 @@ async def type_and_send2(_, message):
     await message.reply_text(response)
     await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
 
-@luna.on_message(filters.regex("@muoimuoibot") & filters.group)
+@luna.on_message(filters.regex("@muoimuoimusicbot") & filters.group)
 async def type_and_send3(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
