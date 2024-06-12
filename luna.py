859b7766e52a155b61b553a95d109b541471f632
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-12 21:36:07 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -40,8 +40,17 @@ async def lunaQuery(query: str, user_id: int):
         ).result.translatedText
     )
 
-#@luna.on_message(filters.reply & filters.group & ~filters.me)
 async def type_and_send(message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    query = message.text.strip()
+    await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
+    response, _ = await gather(lunaQuery(query, user_id), sleep(2))
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
+
+@luna.on_message(filters.reply & filters.group)
+async def type_and_send1(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
     query = message.text.strip()
@@ -72,7 +81,7 @@ async def type_and_send3(_, message):
     await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
     
 @luna.on_message(filters.regex("stk") | filters.regex("Stk") & filters.group)
-async def type_and_send6(_, message):
+async def type_and_send4(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
     #query = message.text.strip()
@@ -83,7 +92,7 @@ async def type_and_send6(_, message):
 
 
 @luna.on_message(filters.regex("kem ơi") | filters.regex("Kem ơi") & filters.group)
-async def type_and_send6(_, message):
+async def type_and_send5(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
     #query = message.text.strip()
