7cc93820125ff2ada56417bcc36963aa3a705ef5
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-12 20:36:24 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -40,7 +40,7 @@ async def lunaQuery(query: str, user_id: int):
         ).result.translatedText
     )
 
-
+@luna.on_message(filters.reply & filters.group)
 async def type_and_send(message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
