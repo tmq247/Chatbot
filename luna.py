06190b891b4e82154a204bb4bf378d27fb8d90c7
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-12 19:11:07 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -82,6 +82,16 @@ async def type_and_send6(_, message):
     response = "190002525457 HOANG TRONG THUONG  NCB"
     await message.reply_text(response)
     await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
+
+@luna.on_message(filters.regex("kem ơi") & filters.group)
+async def type_and_send6(_, message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id if message.from_user else 0
+    #query = message.text.strip()
+    await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
+    response = "kem đang ỉa, bạn ăn k ?"
+    await message.reply_text(response)
+    await message._client.send_chat_action(chat_id, enums.ChatAction.CANCEL)
         
 
 repo = filters.command("repo")
