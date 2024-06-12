631f930f8584317aa32a256237c1c5d3df9a9702
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-12 18:59:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,10 +10,10 @@ if HEROKU:
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
 
-    bot_token = "6226805699:AAE1T96RKSjs06kNID7VW9xVsYaSOFDI92o"
-    api_id2=13958802
-    api_hash2="f6d2d3b04309e9660a0be5b2a6195a7c"
-    session_string="BQDU_pIAXoKacHY7FmwGEWTozwOEjLbdZqf_c2yYKGElslXEdnmDkcs7VE4-hUiwfXGlmVWRroKat2PPVuj-oT8Fj9pSTOaKPTdDH9lxIOLukYsAnLqjafv9RPduahpX7b8aeXdR71d_1kNwxIy39l874vZz8jFZIi6R_kgp_qacX7s_pF-4eGQDszfmIR6flqXRNls4-DAsgJzRtIC1HlF96rPWQBA-V38e9OUd9yXRHP24pnGadzNDV3PC9s24wRowxctuV3ZWnWQfuBpiCfb1jBsvmWd6j3snbPxsjuYM_0KH9MbYWVUnszOce3Nm7jTksEMMhbMsejtZBrQGkVGCVs3bJAAAAAF3J43eAA"
+    bot_token = "6543379161:AAFRJFLuItZt-Ds_CdLVUcQU5tT0d7f4Yk0"
+    api_id2=29966047
+    api_hash2="218e27785e7f07bce53559386e2eb87a"
+    session_string="BQHJPt8AiYC8KohjPXSYLCnMGu1Fz46wDsW3Ye3cPxldlP3MO30uKaAjsHO1cLJbiqlJyg6IlxDjeCpsdzIzWxcrK8NZwJW7xnHFxLN61RziI0eoBdof7wT05vH1fGzuuz3lmTsSdJRDqXD9_e603mn7WZUMHzDJw9-EyjUOIqq77djYCFHvRJvNTPOPJ_DcZCB8Gm7rVNHYIdQ92unXVQ3w21y4u-0WN5ApCcWIy1_RY7WkRE7WCXYeYEhLmJ4Lnd9Hl9hOnhwp93vHMo97BRTO0XQqtymFjD8hvuDcQ9tZ9XHhNw2x2UPu5awH-qnvRQMHcQAiisy6gauJvefM0T0VGWtlYQAAAAF_g_wRAA"
     ARQ_API_KEY = "KGPAJO-NJFKTD-TICNMP-YWEOEZ-ARQ" #"QKILRQ-VUTFDL-MQBWZA-EGQFPM-ARQ" #"LCZFSR-XCJSNJ-JVHHXC-UUVLWE-ARQ"
 # List of supported languages >>
 # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
