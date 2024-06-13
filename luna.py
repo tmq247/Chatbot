fb4b83ee16c10dd9b786804c610fda0da6ac54c4
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-14 04:55:12 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -139,7 +139,7 @@ get = filters.command("get")
 @luna.on_edited_message(get)
 async def get(_, message):
     file_id = message.reply_to_message.animation.file_id
-    print(file_id)
+    print(message)
     await message.reply_text(f"file_id")
 
 repo = filters.command("repo")
