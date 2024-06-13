0ab6590a5831b5ed94ae63b9c64cd8117cca4273
tmq247 <60788746+tmq247@users.noreply.github.com>
2024-06-14 03:16:55 +0700

    Update luna.py

--- luna.py
+++ luna.py
@@ -62,7 +62,7 @@ async def type_and_send(message):
 async def type_and_send0(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id if message.from_user else 0
-    me = reply_to_message.from_user.id
+    me = message.reply_to_message.from_user.id
     print(user_id, me)
     query = message.text.strip()
     await message._client.send_chat_action(chat_id, enums.ChatAction.TYPING)
